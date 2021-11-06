c988ab137416e3b3161ee7375d4078f9ffe25243
Jake Morris <Jakeamorr@gmail.com>
2021-11-05 22:38:42 -0400

    add logic to retreive exercise list from local storage

--- src/components/exerciseList.tsx
+++ src/components/exerciseList.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from "react";
 import { View } from "react-native";
+
 import { IExerciseProps } from "../types/IExercise";
 import Exercise from "./exercise";
 
 const ExerciseList = () => {
-  const exercises: IExerciseProps[] = [
-    {
-      exercise: { name: "test", weight: 150, reps: 8 },
-    },
-    {
-      exercise: { name: "test", weight: 150, reps: 8 },
-    },
-    {
-      exercise: { name: "test", weight: 150, reps: 8 },
-    },
-    {
-      exercise: { name: "test", weight: 150, reps: 8 },
-    },
-  ];
+  const [exercises, setExercises] = useState([]);
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("exerciseList");
+      return value != null ? JSON.parse(value) : null;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getData()
+    .then((val) => {
+      if (val) setExercises(val);
+    })
+    .catch((e) => console.log(e));
 
   return (
     <View>
